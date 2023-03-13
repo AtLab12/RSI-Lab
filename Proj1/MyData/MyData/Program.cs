4f9ab6b5985990d2efff42cb0e10b8bef944c08b
Mikolaj Zawada <m.zawada@bsgroup.eu>
2023-03-13 10:08:48 +0100

    GrpcGreeter

---
+++ Proj1/MyData/MyData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MyData
+{
+    public static void Info()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}, today is {DateTime.Now.ToString("d")}");
+        Console.WriteLine("Current C# version: " + Environment.Version);
+    }
+}
+
+
+namespace RSI {
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            MyData.Info();
+        }
+    }
+}
