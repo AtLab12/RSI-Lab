cf505e9fafb62504919494dc24b3e1564e68da44
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-05-09 08:00:57 +0200

    Add Project5 server

---
+++ Proj6/MyWebService/MyWebService/IRestService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MyWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/xml/People")]
+        List<Person> getAllXml();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/xml/People/{id}", ResponseFormat = WebMessageFormat.Xml)]
+        Person getByIdXml(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/xml/People", Method = "POST", RequestFormat = WebMessageFormat.Xml)]
+        string addXml(Person item);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/xml/People{id}", Method = "DELETE")]
+        string deleteXml(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/xml/People{id}", Method = "PUT", RequestFormat = WebMessageFormat.Xml)]
+        string updateXml(string id, Person person);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/xml/People/count")]
+        int getCountXml();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/People", ResponseFormat = WebMessageFormat.Json)]
+        List<Person> getAllJson();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/People/{id}", ResponseFormat = WebMessageFormat.Json)]
+        Person getByIdJson(string Id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/People", Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string addJson(Person item);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/People{id}", Method = "DELETE", ResponseFormat = WebMessageFormat.Json)]
+        string deleteJson(string Id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/People{id}", Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string updateJson(string Id, Person person);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/People/count")]
+        int getCountJson();
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Person
+    {
+        [DataMember(Order = 0)]
+        public int id { get; set; }
+
+        [DataMember(Order = 1)]
+        public string name { get; set; }
+
+        [DataMember(Order = 2)]
+        public int age { get; set; }
+
+        [DataMember(Order = 3)]
+        public string email { get; set; }
+
+    }
+}
---
+++ Proj6/MyWebService/MyWebService/Service1.svc.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MyWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+
+
+    public class RestService : IRestService
+    {
+        private static List<Person> people = new List<Person>(){
+            new Person() { id = 0, name = "Adam", age = 22, email = "adam@nowak.com"},
+            new Person() { id = 1, name = "Jan", age = 26, email = "kowalski@onet.pl"},
+            new Person() { id = 2, name = "Stefan", age = 55, email = "stef2344123@gmail.com"}
+        };
+
+        public List<Person> getAllXml(){
+            return people;
+        }
+
+        public Person getByIdXml(string Id){
+            int id = int.Parse(Id);
+            int index = people.FindIndex(p => p.id == id);
+            if (index == -1){
+                throw new WebFaultException<string>("404: Not Found", HttpStatusCode.NotFound);
+            }
+            return people.ElementAt(index);
+        }
+
+        public string addXml(Person person){
+            if (person == null){
+                throw new WebFaultException<string>("400: BadRequest", HttpStatusCode.BadRequest);
+            }
+            int maxId = 0;
+            foreach (Person temp in people){
+                if (temp.id > maxId){
+                    maxId = temp.id;
+                }
+            }
+            person.id = maxId+1;
+            people.Add(person);
+            return "Added person: " + person.id + ", " + person.name + ", " + person.age + ", " + person.email;
+        }
+
+        public string deleteXml(string Id)
+        {
+            int id = int.Parse(Id);
+            int index = people.FindIndex(p => p.id == id);
+            if (index == -1){
+                throw new WebFaultException<string>("404: Not Found", HttpStatusCode.NotFound);
+            }
+            Person person = people.ElementAt(index);
+            people.RemoveAt(index);
+            return "Removed person: " + person.id + ", " + person.name + ", " + person.age + ", " + person.email;
+        }
+
+        public string updateXml(string Id, Person person){
+            int id = int.Parse(Id);
+            int index = people.FindIndex(p => p.id == id);
+            if (index == -1){
+                throw new WebFaultException<string>("404: Not Found", HttpStatusCode.NotFound);
+            }
+            Person personToEdit = people.ElementAt(index);
+            personToEdit.name = person.name;
+            personToEdit.age = person.age;
+            personToEdit.email = person.email;
+
+            return "Updated person: " + person.id + ", " + person.name + ", " + person.age + ", " + person.email;
+        }
+
+        public int getCountXml()
+        {
+            return people.Count;
+        }
+
+        public List<Person> getAllJson()
+        {
+            return people;
+        }
+
+        public Person getByIdJson(string Id)
+        {
+            int id = int.Parse(Id);
+            int index = people.FindIndex(p => p.id == id);
+            if (index == -1)
+            {
+                throw new WebFaultException<string>("404: Not Found", HttpStatusCode.NotFound);
+            }
+            return people.ElementAt(index);
+        }
+
+        public string addJson(Person person)
+        {
+            if (person == null)
+            {
+                throw new WebFaultException<string>("400: BadRequest", HttpStatusCode.BadRequest);
+            }
+            int maxId = 0;
+            foreach (Person temp in people)
+            {
+                if (temp.id > maxId)
+                {
+                    maxId = temp.id;
+                }
+            }
+            person.id = maxId + 1;
+            people.Add(person);
+            return "Added person: " + person.id + ", " + person.name + ", " + person.age + ", " + person.email;
+        }
+
+        public string deleteJson(string Id)
+        {
+            int id = int.Parse(Id);
+            int index = people.FindIndex(p => p.id == id);
+            if (index == -1)
+            {
+                throw new WebFaultException<string>("404: Not Found", HttpStatusCode.NotFound);
+            }
+            Person person = people.ElementAt(index);
+            people.RemoveAt(index);
+            return "Removed person: " + person.id + ", " + person.name + ", " + person.age + ", " + person.email;
+        }
+
+        public string updateJson(string Id, Person person)
+        {
+            int id = int.Parse(Id);
+            int index = people.FindIndex(p => p.id == id);
+            if (index == -1)
+            {
+                throw new WebFaultException<string>("404: Not Found", HttpStatusCode.NotFound);
+            }
+            Person personToEdit = people.ElementAt(index);
+            personToEdit.name = person.name;
+            personToEdit.age = person.age;
+            personToEdit.email = person.email;
+
+            return "Updated person: " + person.id + ", " + person.name + ", " + person.age + ", " + person.email;
+        }
+
+        public int getCountJson()
+        {
+            return people.Count;
+        }
+
+    }
+}
