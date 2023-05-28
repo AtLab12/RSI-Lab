3d4366a385fa9683a324ce717bc9d97bcdad1bc6
Mikolaj Zawada <m.zawada@bsgroup.eu>
2023-05-28 14:29:00 +0200

    Proj7

---
+++ Proj7/ReceiveRabbit/ReceiveRabbit/Program.cs
@@ -0,0 +1,33 @@
+﻿// w celu uruchomienia
+//1. brew services start rabbitmq
+//2. Najpierw Receiver
+//3. Potem send
+
+using System.Text;
+using Microsoft.AspNetCore.Connections;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
